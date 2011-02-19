d7ec3109de819a58534cb438bffe8d9555a88bcd
Hayashi Masahiro <mhayashi1120@gmail.com>
2011-02-19 23:30:42 +0900

    update

--- src/haskell/Puyo.hs
+++ src/haskell/Puyo.hs
@@ -52,22 +52,24 @@ getColor :: Block -> Point -> Color
 getColor b (x,y) =  getRow b y !! x
 
 getRow :: Block -> Y -> Row
-getRow (_:bs) y = getRow bs (y - 1)
 getRow (b:_) 0 = b
--- getRow [] _ = TODO
+getRow (_:bs) y = getRow bs (y - 1)
+getRow [] _ = error "getRow"
 
 setColor :: Block -> Point -> Color -> Block
-setColor (_:bs) (x, y) c = setColor bs (x, y - 1) c
 setColor (b:bs) (x, 0) c = setRowColor b x c : bs
--- setColor [] _ _ = TODO
+setColor (_:bs) (x, y) c = setColor bs (x, y - 1) c
+setColor [] _ _ = error "setColor"
 
 setRowColor :: Row -> X -> Color -> Row
+setRowColor (_:rs) 0 c = c : rs
 setRowColor (r:rs) x c = r : setRowColor rs (x - 1) c
-setRowColor (r:rs) 0 c = c : rs
--- setRowColor [] _ _ = TODO
+setRowColor [] _ _ = error "setRowColor"
 
--- collectGroup :: Block -> Point -> PuyoPuyo
--- collectGroup b p = 
+-- collectGroup :: Block -> [Point] -> Point -> PuyoPuyo
+-- collectGroup b h (x,y) = case getColor b (x,y) of 
+--                          ' ' -> []
+--                          x -> getColor b (x,y) 
 
 -- collectGroups :: Block -> [Point] -> [PuyoPuyo]
 
